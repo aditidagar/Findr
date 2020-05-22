50ec18d5198cb3247aa4126d2143e25358e54b41
Ibrahim Fazili <fazili.ibrahim@gmail.com>
2020-05-22 17:26:01 -0400

    fixed overlapping issues and floating action buttons

--- app/assets/styles/index.js
+++ app/assets/styles/index.js
@@ -30,7 +30,8 @@ export default StyleSheet.create({
 		shadowOpacity: 0.05,
 		shadowRadius: 10,
 		shadowColor: BLACK,
-		shadowOffset: { height: 0, width: 0 }
+		shadowOffset: { height: 0, width: 0 },
+		height: DIMENSION_HEIGHT-240,
 	},
 	matchesCardItem: {
 		marginTop: -35,
@@ -75,10 +76,14 @@ export default StyleSheet.create({
 		marginRight: 4
 	},
 	actionsCardItem: {
+		flex: 1,
+    	justifyContent: "flex-end",
 		flexDirection: "row",
 		alignItems: "center",
 		paddingVertical: 20,
-		alignSelf: "center"
+		alignSelf: "center",
+		position: "absolute",
+		bottom: 0,
 	},
 	button: {
 		width: 60,
@@ -307,7 +312,7 @@ export default StyleSheet.create({
 	actionsProfile: {
 		justifyContent: "center",
 		flexDirection: "row",
-		alignItems: "center"
+		alignItems: "center",
 	},
 	iconButton: { fontFamily: ICON_FONT, fontSize: 20, color: WHITE },
 	textButton: {
