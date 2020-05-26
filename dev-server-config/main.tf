6925f82d1e3c3829759519e9d933389be7ff9d8f
Lakshya2610 <lakshyagupta26@live.com>
2020-05-26 18:30:09 -0400

    created terraform configuration for dev infrastrucure

--- dev-server-config/main.tf
+++ dev-server-config/main.tf
@@ -23,12 +23,13 @@ resource "aws_launch_configuration" "find_dev_backend_config" {
               aws configure set aws_access_key_id "${var.access_key}"
               aws configure set aws_secret_access_key "${var.secret_key}"
               aws configure set default_region_name "us-east-1"
-              aws s3 cp s3://findr-user-media/.env ./.env
+              sudo yum install git -y
               git clone https://github.com/Lakshya2610/Findr.git
               cd Findr/backend
+              aws s3 cp s3://findr-user-media/.env ./.env
               npm i
               sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3000
-              NODE_ENV=production node index.js
+              node index.js
               EOF
   
   lifecycle {
@@ -40,11 +41,12 @@ resource "aws_autoscaling_group" "findr_dev_autoscaler" {
 
   launch_configuration = "${aws_launch_configuration.find_dev_backend_config.id}"
   availability_zones = ["${data.aws_availability_zones.all.names}"]
+  depends_on = ["aws_lb.findr-dev-alb"]
 
   min_size = 1
   max_size = 3
 
-  load_balancers = ["${aws_lb.findr-dev-alb.name}"]
+  target_group_arns = ["${aws_lb_target_group.backend_target_group.arn}"]
   health_check_type = "ELB"
 
   tag {
@@ -60,9 +62,26 @@ resource "aws_lb" "findr-dev-alb" {
   load_balancer_type = "application"
 
   security_groups = ["${aws_security_group.findr_dev_server_rules.id}"]
+  subnets = ["${data.aws_subnet_ids.all.ids}"]
 
   tags = {
     Enviroment = "development"
+  }
+}
+
+resource "aws_lb_target_group" "backend_target_group" {
+  name = "backend-target-group"
+  port = 80
+  protocol = "HTTP"
+  vpc_id = "${aws_default_vpc.default.id}"
+
+  health_check {
+    interval = 10
+    path = "/"
+    port = 80
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    matcher = "200-299"
   }
 }
 
@@ -73,16 +92,7 @@ resource "aws_lb_listener" "request_listener" {
 
   default_action {
     type = "forward"
-  }
-}
-
-resource "aws_lb_listener" "ssh_listener" {
-  load_balancer_arn = "${aws_lb.findr-dev-alb.arn}"
-  port = "22"
-  protocol = "TCP"
-
-  default_action {
-    type = "forward"
+    target_group_arn = "${aws_lb_target_group.backend_target_group.arn}"
   }
 }
 
@@ -117,4 +127,8 @@ resource "aws_default_vpc" "default" {
   tags = {
     Name = "Default VPC"
   }
+}
+
+data "aws_subnet_ids" "all" {
+  vpc_id = "${aws_default_vpc.default.id}"
 }
