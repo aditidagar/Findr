3fafbdc0753cdd7844f1a2c1304f5605b1212b7b
Lakshya2610 <lakshyagupta26@live.com>
2020-05-30 13:22:27 -0400

    added docs for some functions in Fetcher

--- app/assets/data/Fetcher.js
+++ app/assets/data/Fetcher.js
@@ -1,6 +1,13 @@
 const ENDPOINT = "http://10.0.2.2"; // goes to localhost from avd
 const PORT = 3000;
 
+
+/**
+ * This class provides methods to interact with our API. If no endpoint or port is provided,
+ * it points to the API server on findr domain on port 80
+ * @param customEndpoint (optional) A custom endpoint that the object should point at when sending requests to the API
+ * @param customPort (optional) Custom port number the object should point at. Defaults to 80
+ */
 class Fetcher {
 
     // need to add credentials to log-in to the backend server
@@ -9,6 +16,11 @@ class Fetcher {
         this.PORT = customPort ? customPort : PORT;
     }
 
+    /**
+     * Send a sign-up request to the API. If succesful, upload the profile picture (if provided) through the signed 
+     * PUT url recieved from the API upon successful sign-up
+     * @param {Object} data Form data obtained from the user on the signup page. Assumes that all fields are valid
+     */
     async requestSignUp(data) {
         return (await fetch(this.ENDPOINT + ":" + String(this.PORT) + "/new-user", {
             method: 'POST',
@@ -20,6 +32,23 @@ class Fetcher {
 
     }
 
+    /**
+     * Send log-in request to the API
+     * @param {{ email: String, password: String}} data log-in data to send to the server for verification
+     * @returns {{ 
+     *      success: Boolean, 
+     *      user: Promise<{
+     *          name: String,
+     *          email: String,
+     *          gender: String,
+     *          uni: String,
+     *          major: String,
+     *          age: Number,
+     *          password: String,
+     *          chats: Array<String>
+     *      }>
+     * }} An object containing the status of request and a promise which resolves to user profile if request was succesful
+     */
     async logIn(data) {
         let logInRes = (await (fetch(this.ENDPOINT + ":" + String(this.PORT) + "/login", {
             method: 'POST',
@@ -36,11 +65,25 @@ class Fetcher {
         return { success: true, user }
     }
 
+    /**
+     * Request the API to send profile cards based on the email provided
+     * @param {String} email E-mail of the user for whom to obtain profile cards for
+     * @returns {Promise<{
+     *      _id: String,
+     *      user_id: String,
+     *      crscodes: Array<String>
+     *      addinfo: String
+     * }>} List of profile cards
+     */
     async fetchCards(email) {        
         return await (await fetch(this.ENDPOINT + ":" + String(this.PORT) 
         + "/fetchProfileCards?email=" + email)).json();
     }
 
+    /**
+     * Request API for user profiles based on the ids provided. Look at 
+     * @param {Array<String>} ids List of user ids
+     */
     async fetchUsersById(ids) {
         let users = await (await fetch(this.ENDPOINT + ":" + String(this.PORT) + "/fetchUsers_id", {
             method: 'POST',
