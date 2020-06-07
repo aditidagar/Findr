e05ffa3e3dbbff9aba230df217f2124752eecf17
Harsh-2420 <harsh2420@gmail.com>
2020-06-07 19:04:57 -0400

    more iOS ui fixes

--- app/App.js
+++ app/App.js
@@ -1,6 +1,10 @@
 import React from "react";
-import { Text } from "react-native";
-import { createBottomTabNavigator, createAppContainer, createStackNavigator } from "react-navigation";
+import { Text, Dimensions } from "react-native";
+import {
+  createBottomTabNavigator,
+  createAppContainer,
+  createStackNavigator,
+} from "react-navigation";
 import styles from "./assets/styles";
 import SignUpScreen from "./containers/SignUp";
 import LogInScreen from "./containers/LogIn";
@@ -11,97 +15,118 @@ import ProfileScreen from "./containers/Profile";
 import Icon from "./components/Icon";
 
 const App = createBottomTabNavigator(
-	{
-		Explore: {
-			screen: HomeScreen,
-			navigationOptions: {
-				tabBarIcon: ({ focused }) => {
-					const iconFocused = focused ? "#2c9c91" : "#4a4949";
-					return (
-						<Text style={[styles.iconMenu, { color: iconFocused, marginTop: "15%" }]}>
-							<Icon name="explore" />
-						</Text>
-					);
-				}
-			}
-		},
-		Matches: {
-			screen: MatchesScreen,
-			navigationOptions: {
-				tabBarIcon: ({ focused }) => {
-					const iconFocused = focused ? "#2c9c91" : "#4a4949"; 
-					return (
-						<Text style={[styles.iconMenu, { color: iconFocused, marginTop: "15%" }]}>
-							<Icon name="heart" />
-						</Text>
-					);
-				}
-			}
-		},
-		Chat: {
-			screen: MessagesScreen,
-			navigationOptions: {
-				tabBarIcon: ({ focused }) => {
-					const iconFocused = focused ? "#2c9c91" : "#4a4949";
-					return (
-						<Text style={[styles.iconMenu, { color: iconFocused, marginTop: "15%" }]}>
-							<Icon name="chat" />
-						</Text>
-					);
-				}
-			}
-		},
-		Profile: {
-			screen: ProfileScreen,
-			navigationOptions: {
-				tabBarIcon: ({ focused }) => {
-					const iconFocused = focused ? "#2c9c91" : "#4a4949";
-					return (
-						<Text style={[styles.iconMenu, { color: iconFocused, marginTop: "15%" }]}>
-							<Icon name="user" />
-						</Text>
-					);
-				}
-			}
-		}
-	},
-	{
-		tabBarOptions: {
-			activeTintColor: "#7444C0",
-			inactiveTintColor: "#363636",
-			labelStyle: {
-				fontSize: 0,
-				textTransform: "uppercase",
-				paddingTop: 10
-			},
-			style: {
-				backgroundColor: "#FFF",
-				borderTopWidth: 0,
-				paddingVertical: 20,
-				height: 50,
-				shadowOpacity: 0.3,
-				shadowRadius: 10,
-				shadowColor: "#000",
-				shadowOffset: { height: 0, width: 0 },
-				elevation: 25
-			}
-		}
-	}
+  {
+    Explore: {
+      screen: HomeScreen,
+      navigationOptions: {
+        tabBarIcon: ({ focused }) => {
+          const iconFocused = focused ? "#2c9c91" : "#4a4949";
+          return (
+            <Text
+              style={[
+                styles.iconMenu,
+                { color: iconFocused, marginTop: "15%" },
+              ]}
+            >
+              <Icon name="explore" />
+            </Text>
+          );
+        },
+      },
+    },
+    Matches: {
+      screen: MatchesScreen,
+      navigationOptions: {
+        tabBarIcon: ({ focused }) => {
+          const iconFocused = focused ? "#2c9c91" : "#4a4949";
+          return (
+            <Text
+              style={[
+                styles.iconMenu,
+                { color: iconFocused, marginTop: "15%" },
+              ]}
+            >
+              <Icon name="heart" />
+            </Text>
+          );
+        },
+      },
+    },
+    Chat: {
+      screen: MessagesScreen,
+      navigationOptions: {
+        tabBarIcon: ({ focused }) => {
+          const iconFocused = focused ? "#2c9c91" : "#4a4949";
+          return (
+            <Text
+              style={[
+                styles.iconMenu,
+                { color: iconFocused, marginTop: "15%" },
+              ]}
+            >
+              <Icon name="chat" />
+            </Text>
+          );
+        },
+      },
+    },
+    Profile: {
+      screen: ProfileScreen,
+      navigationOptions: {
+        tabBarIcon: ({ focused }) => {
+          const iconFocused = focused ? "#2c9c91" : "#4a4949";
+          return (
+            <Text
+              style={[
+                styles.iconMenu,
+                { color: iconFocused, marginTop: "15%" },
+              ]}
+            >
+              <Icon name="user" />
+            </Text>
+          );
+        },
+      },
+    },
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: "#7444C0",
+      inactiveTintColor: "#363636",
+      labelStyle: {
+        fontSize: 0,
+        paddingTop: 0,
+        margin: 0,
+        opacity: 0,
+      },
+      style: {
+        backgroundColor: "#FFF",
+        borderTopWidth: 0,
+        paddingVertical: 20,
+        height: Dimensions.get("window").height * 0.04,
+        shadowOpacity: 0.3,
+        shadowRadius: 10,
+        shadowColor: "#000",
+        shadowOffset: { height: 0, width: 0 },
+        elevation: 25,
+      },
+    },
+  }
 );
 
 const RootStack = createStackNavigator(
-	{
-		AppScreen: { 
-			screen: App 
-		},
-		SignUp: {
-			screen: SignUpScreen
-		},
-		LogIn: {
-			screen: LogInScreen
-		}
-	},
-	{ mode: 'modal', headerMode: 'none' }
+  {
+    AppScreen: {
+      screen: App,
+    },
+    SignUp: {
+      screen: SignUpScreen,
+    },
+    LogIn: {
+      screen: LogInScreen,
+    },
+  },
+  { mode: "modal", headerMode: "none" }
 );
 
 // () => this.props.navigation.navigate('SignUp') on Home if signup/login needed
