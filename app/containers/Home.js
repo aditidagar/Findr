618e89c47e996f736326bdf263cc636b52db7f09
Lakshya2610 <lakshyagupta26@live.com>
2020-06-20 16:22:12 -0400

    updated default navigation to login

--- app/containers/Home.js
+++ app/containers/Home.js
@@ -25,9 +25,8 @@ class Home extends React.Component {
     try {
       let storedEmail = await AsyncStorage.getItem("storedEmail");
       if (storedEmail === null) {
-        this.props.navigation.navigate("SignUp");
+        this.props.navigation.navigate("LogIn");
       }
-      // this.props.navigation.navigate("Onboarding");
     } catch (err) {
       console.log(err);
     }
