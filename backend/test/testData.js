cadddfef014e40c7b9659d657b323ba763ef9909
Aditi Dagar <aditi.dagar@mail.utoronto.ca>
2020-06-27 20:53:57 -0400

    test data

---
+++ backend/test/testData.js
@@ -0,0 +1,72 @@
+ module.exports.testData =
+ [  {
+    name: "Sheldon Cooper",
+    email: "sheldon.cooper@caltech.edu",
+    password: bcrypt.hashSync("Cooper73", 10),
+    gender: "M",
+    uni: "California Institute of Technology",
+    major: "Physics",
+    age: 40,
+    chats: [],
+    keywords: ["CSC209", "MAT224", "PHY136"],
+    bio:
+        "One cries because one is sad. I cry because others are stupid and that makes me sad",
+    blueConnections: [],
+    greenConnections: [],
+    },
+
+    {
+    name: "Leonard Hofstadter",
+    email: "leonard.hofstadter@caltech.edu",
+    password: bcrypt.hashSync("Hofstadter2000", 10),
+    gender: "M",
+    uni: "Princeton University",
+    major: "Physics",
+    age: 38,
+    chats: [],
+    keywords: ["CSC148", "MAT102", "PHY136"],
+    bio:
+        "I'm lactose intolerant and asthmatic.",
+    blueConnections: [],
+    greenConnections: [],
+    },
+
+    {
+    name: "Penny Hofstadter",
+    email: "penny.hofstadter@communitycollege.edu",
+    password: bcrypt.hashSync("penny200", 10),
+    gender: "F",
+    uni: "Some Community College",
+    major: "Theatre",
+    age: 35,
+    chats: [],
+    keywords: ["ECO 100", "STA107"],
+    bio:
+        "Pharmaceutical Sales Representative.",
+    blueConnections: [],
+    greenConnections: [],
+    },
+
+    {
+    name: "Meredith Grey",
+    email: "meredith.grey@seattlegrace.com",
+    password: bcrypt.hashSync("darkandtwisted", 10),
+    gender: "F",
+    uni: "Dartmouth College",
+    major: "Life Sciences",
+    age: 32,
+    chats: [],
+    keywords: ["BIO100", "CHM106", "PHY105"],
+    bio:
+        "Too often, the thing you want most is the one thing you can't have",
+    blueConnections: [],
+    greenConnections: [],
+    },
+
+    
+
+
+]
+    
+
+
