0aacef4835bca8fb2dfe57e4a550332217ead411
Aditi Dagar <aditi.dagar@mail.utoronto.ca>
2020-06-27 21:09:57 -0400

    test data ...

--- backend/test/testData.js
+++ backend/test/testData.js
@@ -63,6 +63,102 @@
     greenConnections: [],
     },
 
+    {
+    name: "Cristina Yang",
+    email: "cristina.yang@seattlegrace.com",
+    password: bcrypt.hashSync("shark", 10),
+    gender: "F",
+    uni: "Stanford University",
+    major: "Life Sciences",
+    age: 34,
+    chats: [],
+    keywords: ["BIO100", "CHM106", "PHY105"],
+    bio:
+        "He's not the sun, you're the sun",
+    blueConnections: [],
+    greenConnections: [],
+    },
+
+    {
+    name: "Derek Shepherd",
+    email: "derek.shepherd@seattlegrace.com",
+    password: bcrypt.hashSync("mcsteamy", 10),
+    gender: "M",
+    uni: "Harvard University",
+    major: "Neuroscience",
+    age: 40,
+    chats: [],
+    keywords: ["BIO100", "MAT224", "ECO100"],
+    bio:
+        "I got hit by a trailer.",
+    blueConnections: [],
+    greenConnections: [],
+    },
+
+    {
+    name: "Owen Hunt",
+    email: "owen.hunt@seattlegrace.com",
+    password: bcrypt.hashSync("ginger", 10),
+    gender: "M",
+    uni: "Brown University",
+    major: "Chemistry",
+    age: 41,
+    chats: [],
+    keywords: ["CSC209", "CSC148", "MAT102"],
+    bio:
+        "Trauma Surgeon, served in Iraq",
+    blueConnections: [],
+    greenConnections: [],
+    },
+
+    {
+    name: "Miranda Bailey",
+    email: "miranda.bailey@seattlegrace.com",
+    password: bcrypt.hashSync("nazi", 10),
+    gender: "F",
+    uni: "",
+    major: "",
+    age: 42,
+    chats: [],
+    keywords: ["STA107", "CSC207", "MAT135"],
+    bio:
+        "I got five rules.",
+    blueConnections: [],
+    greenConnections: [],
+    },
+
+    {
+    name: "Mayim bialik",
+    email: "mayim.bialik@ucla.com",
+    password: bcrypt.hashSync("neildiamond", 10),
+    gender: "F",
+    uni: "",
+    major: "",
+    age: 42,
+    chats: [],
+    keywords: ["POL113", "RLG101"],
+    bio:
+        "A Female Sheldon Cooper.",
+    blueConnections: [],
+    greenConnections: [],
+    },
+
+    {
+        name: "Mayim bialik",
+        email: "mayim.bialik@ucla.com",
+        password: bcrypt.hashSync("neildiamond", 10),
+        gender: "F",
+        uni: "",
+        major: "",
+        age: 42,
+        chats: [],
+        keywords: ["POL113", "RLG101"],
+        bio:
+            "A Female Sheldon Cooper.",
+        blueConnections: [],
+        greenConnections: [],
+        }
+
     
 
 
