6b486da700bf5af04316bea8cc34a8a3d1e9a31a
Ibrahim Fazili <fazili.ibrahim@gmail.com>
2020-06-29 15:44:45 -0400

    starting design now

--- app/containers/OfflinePop.js
+++ app/containers/OfflinePop.js
@@ -1,18 +1,39 @@
 import React, { PureComponent } from 'react';
-import { View, Text, Dimensions, StyleSheet } from 'react-native';
-import styles from "../assets/styles/index"
+import { View, Text, Dimensions, StyleSheet, NetInfo } from 'react-native';
+import styles from "../assets/styles/index";
+// import NetInfo from "@react-native-community/netinfo";
 
 function MiniOfflineSign() {
-  return (
-    <View style={styles.offlineContainer}>
-      <Text style={styles.offlineText}>No Internet Connection</Text>
-    </View>
-  );
-}
-class OfflineNotice extends PureComponent {
-  render() {
-      return <MiniOfflineSign />;
+    return (
+      <View style={styles.offlineContainer}>
+        <Text style={styles.offlineText}>No Internet Connection</Text>
+      </View>
+    );
   }
-}
+  
+  class OfflineNotice extends PureComponent {
+    state = {
+      isConnected: true
+    };
+  
+    componentDidMount() {
+      NetInfo.isConnected.addEventListener('connectionChange', this.handleConnectivityChange);
+    }
+  
+    componentWillUnmount() {
+      NetInfo.isConnected.removeEventListener('connectionChange', this.handleConnectivityChange);
+    }
+  
+    handleConnectivityChange = isConnected => {
+        this.setState({ isConnected });
+    };
+  
+    render() {
+      if (!this.state.isConnected) {
+        return <MiniOfflineSign />;
+      }
+      return null;
+    }
+  }
 
 export default OfflineNotice;
